809644f168cd4a6d48bdd6bcc8eb9bbb6ebdd2c9
Vasilii <vaskar967@mail.ru>
2020-10-23 20:58:45 +0300

    Created site

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menuToggle = document.querySelector('#menu-toggle'),
+	menu = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', (e) => {
+	e.preventDefault()
+	menu.classList.toggle('visible')
+})
--- script.js
+++
@@ -1,2 +0,0 @@
-document.body.innerHTML = '<h1>Hello from Javascript!</h1>'
-document.body.innerHTML = '<h2>Hello from Javascript!</h2>'
